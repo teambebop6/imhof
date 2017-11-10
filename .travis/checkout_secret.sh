5b0a5f03891358087fe5cdb72a44f3d85f4cb5a3
henryhuang <h1886@outlook.com>
2017-11-10 11:20:24 +0800

    modify checkout secret script

--- .travis/checkout_secret.sh
+++ .travis/checkout_secret.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_424b72c7dc47_key -iv $encrypted_424b72c7dc47_iv -in ./travis/id_rsa_deploy_teambebop6.enc -out id_rsa_deploy_teambebop6 -d
+openssl aes-256-cbc -K $encrypted_424b72c7dc47_key -iv $encrypted_424b72c7dc47_iv -in ./.travis/id_rsa_deploy_teambebop6.enc -out id_rsa_deploy_teambebop6 -d
 
 # Start SSH agent
 eval $(ssh-agent -s)
