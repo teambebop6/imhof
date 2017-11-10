ea35ef0fdc034fc788bbbc394eec0d9cd91c4a8b
henryhuang <h1886@outlook.com>
2017-11-10 11:11:17 +0800

    modify checkout secret script

--- .travis/checkout_secret.sh
+++ .travis/checkout_secret.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Decrypt the private key
-openssl aes-256-cbc -K $encrypted_424b72c7dc47_key -iv $encrypted_424b72c7dc47_iv -in id_rsa_deploy_teambebop6.enc -out ~/.ssh/id_rsa -d
-# Set the permission of the key
-chmod 600 ~/.ssh/id_rsa
+openssl aes-256-cbc -K $encrypted_424b72c7dc47_key -iv $encrypted_424b72c7dc47_iv -in id_rsa_deploy_teambebop6.enc -out id_rsa_deploy_teambebop6 -d
 
 # Start SSH agent
-eval $(ssh-agent)
+eval $(ssh-agent -s)
+
+# Set the permission of the key
+chmod 600 id_rsa_deploy_teambebop6
+
 # Add the private key to the system
-ssh-add ~/.ssh/id_rsa
+ssh-add id_rsa_deploy_teambebop6
 
 git clone git@github.com:graspworld/imhof-secret.git ../imhof-secret
