1bd69e75a061846f8bca8b3c1575f6fe2aca0fcc
Florian Rudin <flaudre@gmail.com>
2017-11-10 17:36:35 +0800

    smtp credentials are now loaded from app config

--- helpers/sendmail.js
+++ helpers/sendmail.js
@@ -2,15 +2,14 @@ var nodemailer = require('nodemailer');
 var smtpTransport = require('nodemailer-smtp-transport');
 var transporter = undefined;
 
-transporter = nodemailer.createTransport(smtpTransport({
-  host: 'email-smtp.eu-west-1.amazonaws.com',
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PW
-   }
-}));
-
-
-module.exports = transporter;
+module.exports = function(config){
+  return transporter = nodemailer.createTransport(smtpTransport({
+    host: 'email-smtp.eu-west-1.amazonaws.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: config.SMTP_USER,
+      pass: config.SMTP_PW,
+    }
+  }));
+}
