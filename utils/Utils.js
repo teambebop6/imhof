d2ce38c88c0df7b2a8904fdfd732f4b5d8bd8419
Florian Rudin <flaudre@gmail.com>
2017-11-13 17:03:22 +0800

    --ammend

--- utils/Utils.js
+++ utils/Utils.js
@@ -1,6 +1,18 @@
-exports.ifNoItemsInCookiesThenRedirect = function(req, res, next){
+module.exports.ifNoItemsInCookiesThenRedirect = function(req, res, next){
   if(!req.cookies.itemsToBuy){
     return res.redirect('/shop');
   }
   next();
+}
+
+module.exports.getCartItems = function(cartCookie){
+  var items = [];
+  if(cartCookie){
+    JSON.parse(cartCookie).forEach(function(item){
+      if(item && item.itemsAmount && item._id){
+        items.push(item);
+      }
+    });
+  }
+  return items;
 }
